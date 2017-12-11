87fe7b852b5119e7ba50cc9bc7e549b33a8ba62c
vagrant <vagrant@laravel>
2017-12-11 08:02:43 +0000

    Finish user auth

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -23,8 +23,32 @@
 
             <!-- RIGHT -->
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="#">Log In</a</li>
-                <li><a href="#">Sign Up</a</li>
+                @guest
+                    <li><a href="#">Log In</a</li>
+                    <li><a href="#">Sign Up</a</li>
+                @else
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                            <span class="user-avatar pull-left" style="margin-right:8px; margin-top:-5px;">
+                                <img src="" class="img-responsive img-circle" width="30px" height="30px">
+                            </span>
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+
+                        <ul class="dropdown-menu" role="menu">
+                            <li>
+                                <a href="{{ route('logout') }}"
+                                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                    LogOut
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="post" style="display:none;">
+                                    {{ csrf_field() }}
+                                </form>
+                            </li>
+                        </ul>
+                    </li>
+                @endguest
             </ul>
         </div>
     </div>
