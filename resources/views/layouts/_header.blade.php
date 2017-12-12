e0f7eea9e8fc059a14e6f28209dcd0597fc691f7
vagrant <vagrant@laravel>
2017-12-12 06:01:42 +0000

    added user edit

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -30,16 +30,22 @@
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                             <span class="user-avatar pull-left" style="margin-right:8px; margin-top:-5px;">
-                                <img src="" class="img-responsive img-circle" width="30px" height="30px">
+                                <img src="https://camo.githubusercontent.com/76cf64f8c548080c14b98cb217dde659b1f568c2/68747470733a2f2f312e67726176617461722e636f6d2f6176617461722f34366232646362303936353837623732363339643061363139633361613631633f643d68747470732533412532462532466173736574732d63646e2e6769746875622e636f6d253246696d6167657325324667726176617461727325324667726176617461722d757365722d3432302e706e6726723d7826733d3732" class="img-responsive img-circle" width="30px" height="30px">
                             </span>
                             {{ Auth::user()->name }} <span class="caret"></span>
                         </a>
 
                         <ul class="dropdown-menu" role="menu">
                             <li>
+                                <a href="{{ route('users.edit', Auth::id()) }}">
+                                    编辑资料
+                                </a>
+                            </li>
+
+                            <li>
                                 <a href="{{ route('logout') }}"
                                     onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                    LogOut
+                                    退出登陆
                                 </a>
 
                                 <form id="logout-form" action="{{ route('logout') }}" method="post" style="display:none;">
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -17,10 +17,11 @@
                     <div class="media-body">
                         <hr>
                         <h4><strong>个人简介</strong></h4>
-                        <p> You are oK! </p>
+                        <p> {{ $user->introduction }} </p>
                         <hr>
                         <h4><strong>注册于</strong></h4>
-                        <p>January 01 1901</p>
+
+                        <p> {{ $user->created_at->diffForHumans() }} </p>
                     </div>
                 </div>
             </div>
