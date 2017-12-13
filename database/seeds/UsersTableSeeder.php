05eb0789c3381153692ad66fa6724d700f8c474a
vagrant <vagrant@laravel>
2017-12-13 08:15:58 +0000

    added fake data

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = app(Faker\Generator::class);
+
+        $avatars=[
+            'http://larabbs.dev/uploads/images/avatars/201712/11//1_1513064582_mYE3ykUof7.png'
+        ];
+
+        $users = factory(User::class)
+                        ->times(10)
+                        ->make()
+                        ->each(function($user, $index)
+                            use($faker, $avatars) {
+                                $user->avatar = $faker->randomElement($avatars);
+                            });
+
+        $user_array = $users->makeVisible(['password', 'remember_token'])->toArray();
+
+        User::insert($user_array);
+
+        $user = User::find(1);
+
+        $user->name = 'Test Faker';
+        $user->email = 'test@test.com';
+        $user->avatar = 'http://larabbs.dev/uploads/images/avatars/201712/11//1_1513064582_mYE3ykUof7.png';
+        $user->save();
+    }
+}
