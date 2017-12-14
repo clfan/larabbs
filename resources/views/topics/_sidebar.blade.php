3201fedd585a5e878b59fb7a6a60338fe91eec72
vagrant <vagrant@laravel>
2017-12-14 08:04:07 +0000

    updated topic create

--- resources/views/topics/_sidebar.blade.php
+++ resources/views/topics/_sidebar.blade.php
@@ -1,5 +1,8 @@
 <div class="panel panel-default">
     <div class="panel-body">
-        Right Side Bar !!!
+        <a href="{{ route('topics.create') }}" class="btn btn-success btn-block" aria-label="Left Align">
+            <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+            新建帖子
+        </a>
     </div>
 </div>
